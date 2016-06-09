84ad632e600695db5e9535e0289d9fc3105bfe28
Nikolay Ignatyev <noilgt@gmail.com>
2016-06-10 00:46:18 +0300

    Added route, controller, model for adding new note feature

--- models/standup.server.model.js
+++ models/standup.server.model.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
-
 var Schema = mongoose.Schema;
 
+// Setup Schema
 var standupSchema = new Schema({
   memberName: String,
   project: String,
   workYesterday: String,
+  workToday: String,
   impediment: String,
   createdOn: {
     type: Date,
     default: Date.now
   }
 });
+
+// Export model
+module.exports = mongoose.model('Standup', standupSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var standupCtrl = require('../controllers/standup.server.controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Standup project' });
+});
+
+/* GET New Note page */
+router.get('/newnote', function(req, res) {
+  return standupCtrl.getNote(req, res);
+});
+
+/* POST New Note page*/
+router.post('/newnote', function(req, res) {
+  return standupCtrl.createNote(req, res);
 });
 
 module.exports = router;
